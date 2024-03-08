505b300b744aa63b023c8d00c03341df599605d1
natethemasterman <nathan.moran25@ycdsbk12.ca>
2024-03-08 13:10:06 -0500

    processing 3

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,59 @@
 import processing.core.PApplet;
+import java.util.Random;
 
 public class Sketch extends PApplet {
-	
-	
+
+  String CurrentTime;
+  float MiddleFlowerX = random(0, 400);
+  float MiddleFlowerY = random(0, 400);
+  float MiddleFlowerSize = random(40, 120);
+  float MiddleFlowerDistance = MiddleFlowerSize / 2;
+  
+  
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
     size(400, 400);
-  }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
+  } public void setup() {
     background(210, 255, 173);
-  }
+  } public void draw() {
+    double FlowerY = random(0, 400);
+    double FlowerX = random(0, 400);
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    boolean isGreaterX = FlowerX > 200;
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
-}
+    boolean isGreaterY = FlowerY > 200;
+
+    boolean isGreaterXY = FlowerX && FlowerY;
+
+    if (isGreaterXY) {
+      background(100, 100, 50);
+    }
+    // Called repeatedly, anything drawn on the screen goes here
+    // Stem
+    fill(124, 252, 0);
+    rect(MiddleFlowerX - (MiddleFlowerDistance / 5), MiddleFlowerY + (MiddleFlowerDistance + ((MiddleFlowerDistance / 2) - (MiddleFlowerDistance/3))), MiddleFlowerSize / 5, MiddleFlowerSize * 7);
+
+    // Petals
+    fill(255, 255, 255);
+    ellipse(MiddleFlowerX - MiddleFlowerDistance, MiddleFlowerY - MiddleFlowerDistance, MiddleFlowerSize, MiddleFlowerSize);
+    ellipse(MiddleFlowerX - MiddleFlowerDistance, MiddleFlowerY + MiddleFlowerDistance, MiddleFlowerSize, MiddleFlowerSize);
+    ellipse(MiddleFlowerX + MiddleFlowerDistance, MiddleFlowerY - MiddleFlowerDistance, MiddleFlowerSize, MiddleFlowerSize);
+    ellipse(MiddleFlowerX + MiddleFlowerDistance, MiddleFlowerY + MiddleFlowerDistance, MiddleFlowerSize, MiddleFlowerSize);
+
+    // Middle Flower
+    fill(255, 255, 0);
+    ellipse(MiddleFlowerX, MiddleFlowerY, MiddleFlowerSize, MiddleFlowerSize);
+
+    // Sun
+    fill(255, 210, 12);
+    ellipse(MiddleFlowerX - (MiddleFlowerDistance * 3), MiddleFlowerY - (MiddleFlowerDistance * 3), MiddleFlowerSize / 2, MiddleFlowerSize / 2);
+
+    // Show current time
+    fill(0);
+    textSize(25);
+    CurrentTime = hour() + ":" + minute() + ":" + second();
+    text(CurrentTime, 150, 50);
+  }}
